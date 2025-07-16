720338599836102df1b4fb77bd0dc30a29247c37
Stanley Norcius <norciusstanley@gmail.com>
2025-07-16 05:37:21 -0600

    Update translate.js

--- api/translate.js
+++ api/translate.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         Authorization: `Bearer ${OPENAI_API_KEY}`
       },
       body: JSON.stringify({
-        model: "gpt-4",
+        model: "gpt-3.5",
         messages: [
           {
             role: "system",
